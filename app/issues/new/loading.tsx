1acd45d8fdad561240c036d061a019bc31b71e45
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-22 03:15:17 +0530

    Refactor loading component import

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,11 +1,7 @@
 import { Box } from "@radix-ui/themes";
-import delay from "delay";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
 
 const LoadingCreateNewIssuePage = async () => {
-  await delay(4000);
-
   return (
     <Box className="max-w-xl space-y-3">
       <Skeleton width={"20rem"} />
