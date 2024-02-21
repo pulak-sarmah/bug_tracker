72054d223648c56af4f0f939f31c5f99a2095549
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-22 02:40:50 +0530

    Add loading skeleton to IssueDetailPage

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,25 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
-  return <div>LoadingIssueDetailPage</div>;
+  return (
+    <Box className="max-w-xl">
+      <div>
+        <Skeleton />
+        <Flex gap={"3"} my={"2"}>
+          <Skeleton width="5rem" />
+          <Skeleton width="8rem" />
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <Skeleton count={3} />
+        </Card>
+      </div>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
