cf01fd83eeacf8ad0ff53f206dfc7a83ca39c8c7
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-22 03:14:51 +0530

    Add new components to index.ts

---
+++ app/components/index.ts
@@ -0,0 +1,7 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ErrorMessage from "./ErrorMessage";
+import Link from "./Link";
+import Spinner from "./Spinner";
+import Skeleton from "./Skeleton";
+
+export { ErrorMessage, IssueStatusBadge, Link, Spinner, Skeleton };
