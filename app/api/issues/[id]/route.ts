e031a7bc4af1c77708e964e870b04370458b6650
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-22 14:31:02 +0530

    Add PATCH route for updating an issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,36 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/db";
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "No Issue found" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: issue.id,
+    },
+    data: {
+      title: validation.data.title,
+      description: validation.data.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue, { status: 201 });
+}
