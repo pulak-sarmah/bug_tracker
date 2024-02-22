5eefb4d54c3713e66c355dedc9a5233f9ff9d1e4
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-22 15:27:05 +0530

    fixed caching issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -44,6 +44,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       console.error(error);
       setErr("An unexpected error has occured.");
