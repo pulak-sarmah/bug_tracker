c3b73cd2ad2542fc0ebd7b05807364f83f857ee0
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-23 03:01:35 +0530

    Update validation schemas for issue and patchIssue

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -7,5 +7,26 @@ export const issueSchema = z.object({
     .max(255, "Title must be at most 255 characters long"),
   description: z
     .string()
+    .max(65535)
     .min(1, "Description must be at least 1 character long"),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title must be at least 1 character long")
+    .max(255, "Title must be at most 255 characters long")
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description must be at least 1 character long")
+    .max(65535)
+    .optional(),
+
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
