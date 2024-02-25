4b863b923fc13daaed282089060a3e35521144d5
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-25 19:02:21 +0530

    Add variant prop to Register button

--- app/auth/signIn/Register.tsx
+++ app/auth/signIn/Register.tsx
@@ -141,6 +141,7 @@ const Register = ({
             disabled={loading}
             className="w-full"
             size={"3"}
+            variant="solid"
             type="submit"
           >
             Register
